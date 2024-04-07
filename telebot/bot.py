b89722306363a642124ba4377614b86977f6b372
Beluncho <belov-bk@mail.ru>
2024-04-07 17:56:10 +0300

    add telebot

---
+++ telebot/bot.py
@@ -0,0 +1,55 @@
+import telebot
+from telebot import apihelper
+from parsing import parsing
+from os.path import exists
+
+TOKEN = '6449234059:AAGsswG4haJeV1lPewBwHNk9J2b7K671I4E'
+proxies = {
+    'http': '35.185.196.38:3128',
+    'https': '35.185.196.38:3128',
+}
+
+apihelper.proxy = proxies
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'send "/help" for help\n'
+                          'or text me')
+
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, 'send /parsing for news from pythondigest.ru (about python)\n'
+                          'send /file for data news\n'
+                          'send "any text" for revers "txet yna"')
+
+
+
+@bot.message_handler(commands=['parsing'])
+def parse_site(message):
+    chat_id = message.chat.id
+    news = parsing()
+    for new_s in news[:5]:
+        bot.send_message(chat_id, f'{new_s[0]} - {new_s[1]}')
+
+
+@bot.message_handler(commands=['file'])
+def send_file(message):
+    chat_id = message.chat.id
+    if exists('data.csv'):
+        with open('data.csv', 'r', encoding='utf8') as f:
+            bot.send_document(chat_id, f)
+    else:
+        bot.send_message(chat_id, 'Файл не сформирован. Используйте команду /parsing для его формирования')
+
+
+@bot.message_handler(content_types=['text'])
+def reverse_text(message):
+    text = message.text[::-1]
+    bot.reply_to(message, text)
+
+
+bot.polling()
